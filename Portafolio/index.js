95c429cc77113b298b748e10f08d08dcefe95133
Fernanda Moscoso <carolina.moscosot@gmail.com>
2023-12-28 23:55:05 -0500

    agregar responsive

---
+++ Portafolio/index.js
@@ -0,0 +1,9 @@
+// funcion que muesta el menu responsive
+function responsiveMenu() {
+  var x = document.getElementById("nav");
+  if (x.className === "") {
+    x.className = "responsive";
+  } else {
+    x.className = "";
+  }
+}
