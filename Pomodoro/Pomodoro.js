3efc3db38e027a7e0a254098ca18325407918444
Fernanda Moscoso <carolina.moscosot@gmail.com>
2024-01-03 00:17:30 -0500

    mejoras

--- Pomodoro/Pomodoro.js
+++ Pomodoro/Pomodoro.js
@@ -47,12 +47,11 @@ function renderTasks() {
   tasksContainer.innerHTML = html.join("");
 
   const startButtons = document.querySelectorAll(".task .start-button");
-  startButtons.forEach(button => {
-    button.addEventListener("click", e => {
+  startButtons.forEach(startButton => {
+    startbutton.addEventListener("click", () => {
       if (!timer) {
-        const id = button.getAttribute ('data-id');
-        startButtonHandler(id);
-        button.textContent = "In progress...";
+        startButtonHandler(startButton.getAttribute("data-id"));
+        startButton.textContent = "In progress...";
       }
     });
   });
@@ -67,45 +66,44 @@ function startButtonHandler(id) {
     timerHandler(id);
   }, 1000);
 }
+function timerHandler(id = null) {
+  time--;
+  renderTime();
+  if (time === 0) {
+    markComplete(id);
+    clearInterval(timer);
+    renderTasks();
+    startBreak();
+  }
+}
 
-// function timerHandler(id = null) {
-//   time--;
-//   renderTime();
-//   if (time === 0) {
-//     markComplete(id);
-//     clearInterval(timer);
-//     renderTasks();
-//     startBreak();
-//   }
-// }
+function markComplete(id) {
+  const taskId = tasks.findIndex((task) => task.id === id);
+  tasks[taskId].completed = true;
+}
 
-// function markComplete(id) {
-//   const taskId = tasks.findIndex((task) => task.id === id);
-//   tasks[taskId].completed = true;
-// }
+function startBreak() {
+  time = 1 * 60;
+  document.querySelector("#time #taskName").textContent = "Break";
+  timerBreak = setInterval(timerBreakHandler, 1000);
+}
 
-// function startBreak() {
-//   time = 1 * 60;
-//   document.querySelector("#time #taskName").textContent = "Break";
-//   timerBreak = setInterval(timerBreakHandler, 1000);
-// }
+function timerBreakHandler() {
+  time--;
+  renderTime();
+  if (time === 0) {
+    clearInterval(timerBreak);
+    current = null;
+    document.querySelector("#time #taskName").textContent = "";
+    renderTime();
+  }
+}
 
-// function timerBreakHandler() {
-//   time--;
-//   renderTime();
-//   if (time === 0) {
-//     clearInterval(timerBreak);
-//     current = null;
-//     document.querySelector("#time #taskName").textContent = "";
-//     renderTime();
-//   }
-// }
-
-// function renderTime() {
-//   const timeDiv = document.querySelector("#time #value");
-//   const minutes = parseInt(time / 60);
-//   const seconds = parseInt(time % 60);
-//   timeDiv.textContent = `${minutes < 10 ? "0" : ""}${minutes}:${
-//     seconds < 10 ? "0" : ""
-//   }${seconds}`;
-// }
+function renderTime() {
+  const timeDiv = document.querySelector("#time #value");
+  const minutes = parseInt(time / 60);
+  const seconds = parseInt(time % 60);
+  timeDiv.textContent = `${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
+}
